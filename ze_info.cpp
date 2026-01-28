c3ba25af2c8c277e88ddfdb469ae8ca63530fe34
baodii <di.bao@intel.com>
2026-01-27 17:48:18 -0800

    add gpu number
    Signed-off-by: baodii <di.bao@intel.com>

--- ze_info.cpp
+++ ze_info.cpp
@@ -6,68 +6,79 @@
 int main() {
     zeInit(0);
 
-    // ---- Driver ----
+    // ---- Enumerate drivers ----
     uint32_t driverCount = 0;
     zeDriverGet(&driverCount, nullptr);
     assert(driverCount > 0);
 
     std::vector<ze_driver_handle_t> drivers(driverCount);
     zeDriverGet(&driverCount, drivers.data());
-    ze_driver_handle_t driver = drivers[0];
 
-    // ---- Device ----
-    uint32_t deviceCount = 0;
-    zeDeviceGet(driver, &deviceCount, nullptr);
-    assert(deviceCount > 0);
+    uint32_t gpuCount = 0;
 
-    std::vector<ze_device_handle_t> devices(deviceCount);
-    zeDeviceGet(driver, &deviceCount, devices.data());
-    ze_device_handle_t device = devices[0];
+    // ---- Walk drivers and devices ----
+    for (auto driver : drivers) {
+        uint32_t deviceCount = 0;
+        zeDeviceGet(driver, &deviceCount, nullptr);
+        if (deviceCount == 0)
+            continue;
 
-    // ---- Device properties (topology) ----
-    ze_device_properties_t props{};
-    props.stype = ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES;
-    zeDeviceGetProperties(device, &props);
+        std::vector<ze_device_handle_t> devices(deviceCount);
+        zeDeviceGet(driver, &deviceCount, devices.data());
 
-    uint32_t slices    = props.numSlices;
-    uint32_t subslices = props.numSubslicesPerSlice;
-    uint32_t eus       = props.numEUsPerSubslice;
-    uint32_t threadsEU = props.numThreadsPerEU;
+        for (auto device : devices) {
+            ze_device_properties_t props{};
+            props.stype = ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES;
+            zeDeviceGetProperties(device, &props);
 
-    uint32_t totalEUs      = slices * subslices * eus;
-    uint32_t totalThreads = totalEUs * threadsEU;
+            if (props.type != ZE_DEVICE_TYPE_GPU)
+                continue;
 
-    std::cout << "Device: " << props.name << "\n";
-    std::cout << "Slices: " << slices << "\n";
-    std::cout << "Subslices per slice: " << subslices << "\n";
-    std::cout << "EUs per subslice: " << eus << "\n";
-    std::cout << "Threads per EU: " << threadsEU << "\n";
-    std::cout << "Total EUs: " << totalEUs << "\n";
-    std::cout << "Total HW threads: " << totalThreads << "\n";
+            std::cout << "==============================\n";
+            std::cout << "GPU " << gpuCount++ << "\n";
+            std::cout << "Name: " << props.name << "\n";
 
-    // ---- Memory properties ----
-    uint32_t memCount = 0;
-    zeDeviceGetMemoryProperties(device, &memCount, nullptr);
-    assert(memCount > 0);
-    
-    std::vector<ze_device_memory_properties_t> memProps(memCount);
-    for (auto &m : memProps) {
-        m.stype = ZE_STRUCTURE_TYPE_DEVICE_MEMORY_PROPERTIES;
+            // ---- Topology ----
+            uint32_t slices    = props.numSlices;
+            uint32_t subslices = props.numSubslicesPerSlice;
+            uint32_t eus       = props.numEUsPerSubslice;
+            uint32_t threadsEU = props.numThreadsPerEU;
+
+            uint32_t totalEUs      = slices * subslices * eus;
+            uint32_t totalThreads = totalEUs * threadsEU;
+
+            std::cout << "Slices: " << slices << "\n";
+            std::cout << "Subslices per slice: " << subslices << "\n";
+            std::cout << "EUs per subslice: " << eus << "\n";
+            std::cout << "Threads per EU: " << threadsEU << "\n";
+            std::cout << "Total EUs: " << totalEUs << "\n";
+            std::cout << "Total HW threads: " << totalThreads << "\n";
+
+            // ---- Memory heaps (size only, portable) ----
+            uint32_t memCount = 0;
+            zeDeviceGetMemoryProperties(device, &memCount, nullptr);
+
+            std::vector<ze_device_memory_properties_t> memProps(memCount);
+            for (auto &m : memProps) {
+                m.stype = ZE_STRUCTURE_TYPE_DEVICE_MEMORY_PROPERTIES;
+            }
+
+            zeDeviceGetMemoryProperties(device, &memCount, memProps.data());
+
+            std::cout << "Memory heaps:\n";
+            for (uint32_t i = 0; i < memCount; ++i) {
+                const auto &m = memProps[i];
+                std::cout << "  Heap " << i << ":\n";
+                std::cout << "    Name: " << m.name << "\n";
+                std::cout << "    Size (GB): "
+                          << (double)m.totalSize / (1024.0 * 1024.0 * 1024.0)
+                          << "\n";
+            }
+        }
     }
-    
-    zeDeviceGetMemoryProperties(device, &memCount, memProps.data());
-    
-    std::cout << "\nMemory heaps:\n";
-    for (uint32_t i = 0; i < memCount; ++i) {
-        const auto &m = memProps[i];
-    
-        std::cout << "Heap " << i << ":\n";
-        std::cout << "  Name: " << m.name << "\n";
-        std::cout << "  Size (bytes): " << m.totalSize << "\n";
-        std::cout << "  Size (GB): "
-                  << (double)m.totalSize / (1024.0 * 1024.0 * 1024.0)
-                  << "\n";
-    }
+
+    std::cout << "==============================\n";
+    std::cout << "Total GPUs detected: " << gpuCount << "\n";
 
     return 0;
 }
